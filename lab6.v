863e93959e13efeb6fc2deb3b1f6b9da6296f0d0
Crystal Yee Shuen Lim <limcryst@IITS-B473-03.utsc-labs.utoronto.ca>
2017-07-04 17:38:45 -0400

    Tried to fix lab 6's vga output for creating more than 1x1 size squares on the screen

--- lab6.v
+++ lab6.v
@@ -6,7 +6,7 @@
 
 // Part 2 skeleton
 
-module lab6p2
+module lab6
 	(
 		CLOCK_50,						//	On Board 50 MHz
 		// Your inputs and outputs here
@@ -46,6 +46,8 @@ module lab6p2
 	wire [7:0] x;
 	wire [6:0] y;
 	wire writeEn;
+	wire [7:0] x_offset;
+	wire [6:0] y_offset;
 
 			
 	// Put your code here. Your code should produce signals x,y,colour and writeEn/plot
@@ -55,11 +57,11 @@ module lab6p2
 	
 	
    // Instansiate FSM control
-   control c0(.clk(CLOCK_50), .resetn(resetn), .go(KEY[3]), .update(KEY[1]), .ld_x(ld_x), .ld_y(ld_y), .writeEn(writeEn));
+   control c0(.clk(CLOCK_50), .resetn(resetn), .go(KEY[3]), .update(KEY[1]), .ld_x(ld_x), .ld_y(ld_y), .x_offset(x_offset), .y_offset(y_offset), .writeEn(writeEn));
    
 	
    // Instansiate datapath
-	datapath d0(.clk(CLOCK_50), .in(SW[6:0]), .ld_x(ld_x), .ld_y(ld_y), .clr(SW[9:7]), .reset_n(resetn), .xOUT(x), .yOUT(y), .clrOUT(colour));
+	datapath d0(.clk(CLOCK_50), .in(SW[6:0]), .ld_x(ld_x), .ld_y(ld_y), .x_offset(x_offset), .y_offset(y_offset), .clr(SW[9:7]), .reset_n(resetn), .xOUT(x), .yOUT(y), .clrOUT(colour));
 
 	
 
@@ -77,7 +79,7 @@ module lab6p2
 					/* Signals for the DAC to drive the monitor. */
 					.VGA_R(VGA_R),
 					.VGA_G(VGA_G),
-					.VGAl_B(VGA_B),
+					.VGA_B(VGA_B),
 					.VGA_HS(VGA_HS),
 					.VGA_VS(VGA_VS),
 					.VGA_BLANK(VGA_BLANK_N),
@@ -96,6 +98,8 @@ module datapath(input clk,
 	input [6:0] in, // 7 bits input
 	input ld_x,      // 1 bit signal to tell you to load the x value 
 	input ld_y, 	 // 1 bit signal to tell you to load the y value
+	input x_offset,
+	input y_offset,
 	input [2:0] clr, // 3 bit colour
 	input reset_n,
 
@@ -105,23 +109,23 @@ module datapath(input clk,
 );
 
 
+
 always@(posedge clk) begin
    clrOUT <= clr;
 	if(!reset_n) begin
 		xOUT <= 8'b0;
 		yOUT <= 7'b0;
 		
-		
 	end
+
 	else begin
 		if(ld_x)
-			xOUT <= {1'b0, in};
-			
+			xOUT <= {1'b0, in} + x_offset;  // Add extra bit for xIN
 		if(ld_y)
-			yOUT <= in;
-		
+			yOUT <= in + y_offset;
 
 	end
+
 end
 
 endmodule
@@ -137,19 +141,24 @@ module control(
 	 input update,
 
     output reg ld_x, ld_y,
+	 output reg x_offset, y_offset;
     output reg writeEn // tells screen when to update
     );
 
     reg [5:0] current_state, next_state; 
+    reg next_sq;
     
     localparam  S_LOAD_X        = 5'd0,
                 S_LOAD_X_WAIT   = 5'd1,
                 S_LOAD_Y        = 5'd2,
                 S_LOAD_Y_WAIT   = 5'd3,
                 S_CYCLE_0       = 5'd4,
-					 S_CYCLE_1       = 5'd5;
-
-    // Next state logic aka our state table
+			S_CYCLE_1       = 5'd5;
+		
+	 assign x_offset = 8'b0;
+	 assign y_offset = 7'b0;
+	 
+	 // Next state logic aka our state table
     always@(*)
     begin: state_table 
             case (current_state)
@@ -157,8 +166,10 @@ module control(
                 S_LOAD_X_WAIT: next_state = go ? S_LOAD_X_WAIT : S_LOAD_Y; // Loop in current state until go signal goes low
                 S_LOAD_Y: next_state = go ? S_LOAD_Y_WAIT : S_LOAD_Y; // Loop in current state until value is input
                 S_LOAD_Y_WAIT: next_state = go ? S_LOAD_Y_WAIT : S_CYCLE_0; // Loop in current state until go signal goes low
-					 S_CYCLE_0: next_state = update ? S_CYCLE_1 : S_CYCLE_0;
-                S_CYCLE_1: next_state = S_LOAD_X; // we will be done our two operations, start over after
+					 
+					 S_CYCLE_0: next_state = update ? S_CYCLE_1 : S_CYCLE_0;  // UPDATE signal starts drawing the sq
+					 S_CYCLE_1: next_state = S_CYCLE_2;  
+                S_CYCLE_2: next_state = next_px ? S_CYCLE_1 : S_LOAD_X; // IF DONE DRAWING all squares, start over afte, ELSE draw the next square
             default:     next_state = S_LOAD_X;
         endcase
     end // state_table
@@ -179,9 +190,28 @@ module control(
             S_LOAD_Y: begin
                 ld_y = 1'b1;
                 end
+				
             S_CYCLE_1: begin
                 writeEn = 1'b1;
                 end
+				S_CYCLE_2: begin
+					 
+					 // output offset values
+					 if (x_offset < sz)
+						x_offset = x_offset + 1'b1;
+						next_px = 1'b1;
+					else
+						x_offset = 0;
+						if(y_offset < sz)
+							y_offset = y_offset + 1'b1;
+							next_px = 1'b1;
+						else
+							// reset y
+							y_offset = 0;
+							// Go to Load x
+							next_px = 1'b0;
+					 
+					 end
                 
         // default:    // don't need default since we already made sure all of our outputs were assigned a value at the start of the always block
         endcase
@@ -198,4 +228,7 @@ module control(
     end // state_FFS
 
 endmodule
+
+
+
 
